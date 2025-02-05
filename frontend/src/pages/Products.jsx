3769626d39c38e9e4d78e332df51ba89f19e3165
mishrabhi <asmyselfabhishek00@gmail.com>
2025-02-06 01:47:46 +0530

    updated products page

--- frontend/src/pages/Products.jsx
+++ frontend/src/pages/Products.jsx
@@ -1,28 +1,35 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import ProductCard from "../components/productCart";
+import { useAuth } from "../context/AuthContext";
 
 const Products = () => {
   const [products, setProducts] = useState([]);
+  const { token } = useAuth();
+  const navigate = useNavigate();
 
   useEffect(() => {
-    axios
-      .get("http://localhost:5000/api/products")
-      .then((res) => setProducts(res.data));
-  }, []);
+    if (!token) {
+      navigate("/login");
+    } else {
+      axios
+        .get("http://localhost:5000/api/products", {
+          headers: { Authorization: `Bearer ${token}` },
+        })
+        .then((response) => setProducts(response.data))
+        .catch((error) => console.error("Error fetching products", error));
+    }
+  }, [token, navigate]);
 
   return (
-    <div className="p-6 grid grid-cols-2 md:grid-cols-4 gap-4">
-      {products.map((product) => {
-        <Link
-          key={product.id}
-          to={`/products/${product.id}`}
-          className="border p-4"
-        >
-          <h3 className="text-lg">{product.name}</h3>
-          <p className="text-gray-600">{product.price}</p>
-        </Link>;
-      })}
+    <div className="p-6">
+      <h2 className="text-2xl font-bold mb-4">Products</h2>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
+        {products.map((product) => (
+          <ProductCard key={product._id} product={product} />
+        ))}
+      </div>
     </div>
   );
 };
