fb3ffbce6e241c193407b7b6b5e142f3b89cff47
mishrabhi <asmyselfabhishek00@gmail.com>
2025-02-05 18:22:14 +0530

    updated index routes

--- Backend/server.js
+++ Backend/server.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const cors = require("cors");
 const connect = require("./config/db");
+const authRouter = require("./routes/authRoutes");
+const productRouter = require("./routes/productRoutes");
+const cartRouter = require("./routes/cartRoutes");
+const purchaseRouter = require("./routes/purchaseRoutes");
 
 const app = express();
 app.use(express.json());
@@ -8,6 +12,13 @@ app.use(cors());
 
 const PORT = process.env.PORT || 5000;
 
+//routes
+app.use("/api/auth", authRouter);
+app.use("/api/products", productRouter);
+app.use("/api/cart", cartRouter);
+app.use("/api/purchase", purchaseRouter);
+
+//server
 app.listen(PORT, async () => {
   await connect();
   console.log(`Server up and running on port: ${PORT}`);
